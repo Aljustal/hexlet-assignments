e733cb64b1ebc86e19ae2f5c6559efe9dcf9d644
@hexlet/cli <support@hexlet.io>
2022-12-09 14:50:16 +0300

    submit java-spring-ru/spring-orm

---
+++ java-spring-ru/spring-orm/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,52 @@
+package exercise.controller;
+
+import exercise.model.Person;
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    // Автоматически заполняем значение поля
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person getPerson(@PathVariable long id) {
+        return this.personRepository.findById(id);
+    }
+
+    @GetMapping(path = "")
+    public Iterable<Person> getPeople() {
+        return this.personRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    public void addPerson(@RequestBody Person person) {
+        this.personRepository.save(person);
+    }
+    @DeleteMapping(path = "/{id}")
+    public void deletePerson(@PathVariable long id) {
+        this.personRepository.deleteById(id);
+    }
+    @PatchMapping(path = "/{id}")
+    public void updatePerson(@PathVariable long id, @RequestBody Person person) {
+        Person personToUpdate = this.personRepository.findById(id);
+        personToUpdate.setFirstName(person.getFirstName());
+        personToUpdate.setLastName(person.getLastName());
+        this.personRepository.save(personToUpdate);
+    }
+    // END
+}
