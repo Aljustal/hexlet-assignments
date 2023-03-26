9cd13ba5bba04d71fcf00af3e69b2b1088e8f124
@hexlet/cli <support@hexlet.io>
2023-03-27 02:44:08 +0300

    submit java-advanced-ru/test-containers

---
+++ java-advanced-ru/test-containers/src/test/java/exercise/AppTest.java
@@ -0,0 +1,88 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import org.springframework.http.MediaType;
+
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.Container;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+// BEGIN
+// Аннотация позволяет автоматически запускать и останавливать в тестах все контейнеры
+@Testcontainers
+// Все тесты выполняем в транзакции
+@Transactional
+// END
+public class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // BEGIN
+    // Аннотация отмечает контейнер, который будет автоматически запущен
+    @Container
+    // Создаём контейнер с СУБД PostgreSQL
+    // В конструктор передаём имя образа, который будет скачан с Dockerhub
+    // Если не указать версию, будет скачана последняя версия образа
+    private static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres")
+            // Создаём базу данных с указанным именем
+            .withDatabaseName("jdbc:postgresql://localhost:5432/hexlet")
+            // Указываем имя пользователя и пароль
+            .withUsername("sa")
+            .withPassword("sa")
+            // Скрипт, который будет выполнен при запуске контейнера и наполнит базу тестовыми данными
+            .withInitScript("src/test/resources/init.sql");
+
+    // Так как мы не можем знать заранее, какой URL будет у базы данных в контейнере
+    // Нам потребуется установить это свойство динамически
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        // Устанавливаем URL базы данных
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        // Имя пользователя и пароль для подключения
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+        // Эти значения приложение будет использовать при подключении к базе данных
+    }
+    // END
+
+    @Test
+    void testCreatePerson() throws Exception {
+        MockHttpServletResponse responsePost = mockMvc
+            .perform(
+                post("/people")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"firstName\": \"Jackson\", \"lastName\": \"Bind\"}")
+            )
+            .andReturn()
+            .getResponse();
+
+        assertThat(responsePost.getStatus()).isEqualTo(200);
+
+        MockHttpServletResponse response = mockMvc
+            .perform(get("/people"))
+            .andReturn()
+            .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
+        assertThat(response.getContentAsString()).contains("Jackson", "Bind");
+    }
+}
